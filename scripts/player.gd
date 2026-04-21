d3d52680fac3fcc1da3c0a9df3e6fe13611e6731
Micah-Bayley <Micah.Bayley@stu.crc-lennox.qc.ca>
2026-04-21 16:23:09 -0400

    Implemented player double jump.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,25 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
 
+var jumps_left = MAX_JUMPS
+var dash_meter = 100.0 # might change
+
+var is_dashing = false
+var can_dash = true
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	if is_on_floor():
+		jumps_left = MAX_JUMPS
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumps_left > 1:
 		velocity.y = JUMP_VELOCITY
+		jumps_left -= 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +29,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_dash_duration_timeout() -> void:
+	$DashDuration.stop()
+	is_dashing = false
+
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
