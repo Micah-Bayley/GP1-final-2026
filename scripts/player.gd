6530d1c3c474bb8135e9206576bf2577a8667a03
Micah-Bayley <Micah.Bayley@stu.crc-lennox.qc.ca>
2026-04-29 15:13:23 -0400

    Completed finite state machine + player animation
    to do: implement dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,10 +9,13 @@ var dash_meter = 100.0 # might change
 
 var is_dashing = false
 var can_dash = true
+var is_dying = false
 
-enum State { IDLE, RUNNING, JUMPING, DASHING, DYING, ATTACKING }
+enum State { IDLE, RUNNING, JUMPING, FALLING, DASHING, DYING, ATTACKING }
+var active_state = State.IDLE
 
 enum PowerState { NORMAL, INVINCIBLE, FAST, INFDASH }
+var active_powerstate = PowerState.NORMAL
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -31,9 +34,47 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	update_state()
+	update_animation()
 	move_and_slide()
 
+func update_state():
+	if is_dying:
+		active_state = State.DYING
+		return
+	
+	if abs(velocity.x) > 0.05 and is_on_floor():
+		active_state = State.RUNNING
+		return
+		
+	if not is_on_floor():
+		if velocity.y < -0.05:
+			active_state = State.JUMPING
+			return
+		elif velocity.y > 0.05:
+			active_state = State.FALLING
+			return
+	
+	active_state = State.IDLE
+	return
+
+func update_animation():
+	match active_state:
+		State.IDLE:
+			$AnimationPlayer.play("idle")
+		State.RUNNING:
+			$AnimationPlayer.play("running")
+		State.JUMPING:
+			$AnimationPlayer.play("jumping")
+		State.FALLING:
+			$AnimationPlayer.play("falling")
+		State.DASHING:
+			$AnimationPlayer.play("dashing")
+		State.ATTACKING:
+			$AnimationPlayer.play("attacking")
+		State.DYING:
+			$AnimationPlayer.play("dying")
 
 func _on_dash_duration_timeout() -> void:
 	$DashDuration.stop()
